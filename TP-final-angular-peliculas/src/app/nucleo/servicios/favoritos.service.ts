53d751f3246f44706a348f415c09f75491dc2bed
jobaumgertner <josefinabaumgertner@hotmail.com>
2024-10-25 16:53:14 -0300

    servicio peliculas

--- TP-final-angular-peliculas/src/app/nucleo/servicios/favoritos.service.ts
+++ TP-final-angular-peliculas/src/app/nucleo/servicios/favoritos.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritosService {
+  private apiUrl = ""
 
-  constructor() { }
+  constructor(private HTTP: HttpClient) {}
+
+  getPeliculas(): Observable<any[]> {
+
+  }
 }
