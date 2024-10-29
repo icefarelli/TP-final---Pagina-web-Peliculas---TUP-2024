6d982f3ae02f75b7fd34c58fe5c9e022c6926354
icefarelli <ivan.cefarelli@docentes.mdp.utn.edu.ar>
2024-10-29 14:46:36 -0300

    importaciones a app component y styles

--- TP-final-angular-peliculas/src/app/app.component.ts
+++ TP-final-angular-peliculas/src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService, Usuario } from './nucleo/servicios/auth.service';
+import { AuthDialogComponent } from './compartido/auth-dialog/auth-dialog/auth-dialog.component';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AlertComponent } from './compartido/alert/alert.component';
+import { NavbarComponent } from './compartido/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AlertComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TP-final-angular-peliculas';
+
+
+  usuarioActual$: Observable<Usuario | null>; // Cambia a Observable<Usuario | null>
+
+  private authDialogRef: ComponentRef<AuthDialogComponent> | null = null;
+
+  constructor(private authService: AuthService, private viewContainerRef: ViewContainerRef) {
+    // Inicializar usuarioActual$ en el constructor
+    this.usuarioActual$ = this.authService.getUsuarioActual();
+
+    // Escuchar el evento de cierre del diálogo
+    window.addEventListener('cerrarDialog', () => {
+      this.cerrarAuthDialog();
+    });
+  }
+
+  mostrarAuthDialog() {
+    this.cerrarAuthDialog();
+    this.authDialogRef = this.viewContainerRef.createComponent(AuthDialogComponent);
+  }
+
+  cerrarAuthDialog() {
+    if (this.authDialogRef) {
+      this.authDialogRef.destroy();
+      this.authDialogRef = null;
+    }
+  }
+
+  cerrarSesion() {
+    this.authService.cerrarSesion();
+  }
+
 }
--- TP-final-angular-peliculas/src/app/app.routes.ts
+++ TP-final-angular-peliculas/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ListarPeliculasComponent } from './funcionalidades/peliculas/listar-peliculas/listar-peliculas.component';
+import { DetallePeliculasComponent } from './funcionalidades/peliculas/detalle-peliculas/detalle-peliculas.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListaFavoritosComponent } from './funcionalidades/favoritos/lista-favoritos/lista-favoritos.component';
+import { AdministrarReseniasComponent } from './funcionalidades/resenias/administrar-resenias/administrar-resenias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    component: HomeComponent, pathMatch: 'full'
+  },
+  { path: 'peliculas',
+    component: ListarPeliculasComponent
+  },
+  { path: 'pelicula/:id',
+    component: DetallePeliculasComponent
+  },
+  {
+    path:'favoritos',
+    component : ListaFavoritosComponent
+  },
+  {
+    path: 'resenas',
+    component: AdministrarReseniasComponent
+
+  }
+];
