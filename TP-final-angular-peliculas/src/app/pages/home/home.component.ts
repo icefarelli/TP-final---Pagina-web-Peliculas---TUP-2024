dbdb00c3b1e0b1c5407d0de6848613ac68b0371e
jobaumgertner <josefinabaumgertner@hotmail.com>
2024-10-29 14:28:03 -0300

    creacion navbar y pagina home

---
+++ TP-final-angular-peliculas/src/app/pages/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { PeliculasService } from '../../nucleo/servicios/peliculas.service';
+import { Pelicula, PeliculaResponse } from '../../nucleo/modelos/pelicula.interface';
+import { RouterLink } from '@angular/router';
+import { ListarPeliculasComponent } from '../../funcionalidades/peliculas/listar-peliculas/listar-peliculas.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  pelicula?: PeliculaResponse;
+
+
+  constructor(
+    private route: Router,
+    private peliculasService: PeliculasService
+  ) {}
+
+
+
+  explorarPeliculas(): void {
+    this.route.navigate(['/peliculas']);
+  }
+}
+
+
+
