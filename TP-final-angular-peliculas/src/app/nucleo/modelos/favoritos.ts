5901758a265b10a45b34ae89925aa4e7ba7bf489
icefarelli <ivan.cefarelli@docentes.mdp.utn.edu.ar>
2024-11-16 20:01:56 -0300

    Correccion de estructura de proyecto

--- TP-final-angular-peliculas/src/app/nucleo/modelos/favoritos.ts
+++
@@ -1,9 +0,0 @@
-import { Pelicula } from "./pelicula.interface";
-
-export interface Favoritos {
-  id: string;
-  userId: number;
-  nombre: string;
-  descripcion: string;
-  peliculas: Pelicula[];
-}
